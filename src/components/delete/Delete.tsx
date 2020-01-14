2f1a7f694bf9098841eba7fbbd0dfad1861f3ff4
Riccardo Vecchi <rvecchi@byte-code.com>
2020-01-14 21:37:43 +0100

    Style delete

--- src/components/delete/Delete.tsx
+++ src/components/delete/Delete.tsx
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
 import { deleteItem } from '../search/ducks/actions';
+import './Delete.css'
 
 export default class Delete extends Component<{ id: number, deleteItem: Function }> {
 
     render() {
         let { id } = this.props;
         return (
-            <div onClick={()=>deleteItem(id)}>
+            <div className="Delete" onClick={()=>deleteItem(id)}>
                 Delete recipes
             </div>
         );
